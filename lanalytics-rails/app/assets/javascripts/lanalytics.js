45cf564ba858d39091e79576ed2d416094f987c5
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2014-09-16 15:18:16 +0200

    Organize project structure

---
+++ lanalytics-rails/app/assets/javascripts/lanalytics.js
@@ -0,0 +1,8 @@
+// Including the files necessary for the Lanalytics service
+
+//= require lanalytics/lanalytics.framework.js.coffee
+//= require_directory ./lanalytics/plugins
+//= stub lanalytics/plugins/lanalytics.plugins.newplugintemplate.js.coffee
+//= stub lanalytics/plugins/example.lanalytics.plugins.html5videoplayer.js.coffee
+
+lanalytics = new Lanalytics();
--- lanalytics-rails/app/assets/javascripts/lanalytics/lanalytics.js
+++
@@ -1,8 +0,0 @@
-// Including the files necessary for the Lanalytics service
-
-//= require ./lanalytics.framework.js.coffee
-//= require_directory ./plugins
-//= stub ./plugins/lanalytics.plugins.newplugin.js.coffee.template
-//= stub ./plugins/example.lanalytics.plugins.html5videoplayer.js.coffee
-
-lanalytics = new Lanalytics();
