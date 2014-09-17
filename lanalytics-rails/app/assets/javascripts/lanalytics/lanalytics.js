eaed906f6b29a22b55eb48b0b91d2a3dceb9c667
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2014-09-17 11:59:47 +0200

    IMPORTANT commit of javascripts/lanalytics/lanalytics.js

---
+++ lanalytics-rails/app/assets/javascripts/lanalytics/lanalytics.js
@@ -0,0 +1,8 @@
+// Including the files necessary for the Lanalytics service
+
+//= require ./lanalytics.framework.js.coffee
+//= require_directory ./plugins
+//= stub ./plugins/lanalytics.plugins.newplugintemplate.js.coffee
+//= stub ./plugins/example.lanalytics.plugins.html5videoplayer.js.coffee
+
+lanalytics = new Lanalytics();
