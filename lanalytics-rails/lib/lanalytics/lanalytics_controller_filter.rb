239b64b2094eb196e4b1d74dfc810c81e20fd517
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2014-10-11 17:54:49 +0200

    Refactored some tracking controller files

--- lanalytics-rails/lib/lanalytics/lanalytics_controller_filter.rb
+++ lanalytics-rails/lib/lanalytics/lanalytics_controller_filter.rb
@@ -1,4 +1,5 @@
 require 'action_controller'
+require 'gon'
 
 module Lanalytics
   module LanalyticsControllerFilter
--- lanalytics-rails/spec/controllers/tracking_controller_spec.rb
+++ lanalytics-rails/spec/controllers/tracking_controller_spec.rb
@@ -11,33 +11,33 @@ RSpec.describe Lanalytics::TrackingController, :type => :controller do
     
     it "returns http success" do
       post('track', request_data)
-      response.should be_success
+      expect(response).to be_success
     end
 
     it "should push a message into the RabbitMQ Queue" do
       expect(Msgr).to receive(:publish) do | marshalled_exp_api_stmt, msgr_params |
 
         expect(marshalled_exp_api_stmt).to be_an_instance_of(String)
-        puts "#{marshalled_exp_api_stmt}"
         exp_api_stmt = Marshal.load(marshalled_exp_api_stmt)
         expect(exp_api_stmt).to be_an_instance_of(Lanalytics::Model::ExpApiStatement)
         expect(exp_api_stmt.user.uuid).to eq('00000001-3100-4444-9999-000000000001')
-        expect(exp_api_stmt.ressource.type).to eq('Item')
-        expect(exp_api_stmt.ressource.uuid).to eq('00000003-3100-4444-9999-000000000003')
+        expect(exp_api_stmt.verb.type).to eq(:video_play)
+        expect(exp_api_stmt.resource.type).to eq(:Item)
+        expect(exp_api_stmt.resource.uuid).to eq('00000003-3100-4444-9999-000000000003')
 
         expect(msgr_params).to include(:to => 'lanalytics.event.stream')
       end
       post('track', request_data)
-      response.should be_success
+      expect(response).to be_success
     end
   end
 
   def request_data
 
     return {
-      'actor' => { 'uuid' => "00000001-3100-4444-9999-000000000001" },
-      'verb' => { "verb_key" => "video_play", "verb_id" => "http://lanalytics.open.hpi.de/expapi/verbs/video-play" },
-      'ressource' => { "type" => "Item", "uuid" => "00000003-3100-4444-9999-000000000003" }
+      'user' => { 'uuid' => "00000001-3100-4444-9999-000000000001" },
+      'verb' => { "type" => "video_play", "verb_id" => "http://lanalytics.open.hpi.de/expapi/verbs/video-play" },
+      'resource' => { "type" => "Item", "uuid" => "00000003-3100-4444-9999-000000000003" }
     }
   end
 
