0609deba862e71fabaffa5e3ec78051afc1ee305
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2014-10-21 16:11:30 +0200

    Commiting missing files to handle the routing of rabbit events

--- app/consumer/lanalytics_resource_consumer.rb
+++ app/consumer/lanalytics_resource_consumer.rb
@@ -1,30 +1,24 @@
 class LanalyticsResourceConsumer < Msgr::Consumer
 
   def create
-    puts "Nachricht angekommen #{payload.inspect}"
-    # Lanalytics::Processing.new(
-    #   Lanalytics.Processor::Neo4jProcessor.new(),
-
-    #   Lanalytics.Processor::Neo4jProcessor.new()
-    #   Lanalytics.Processor::Neo4jProcessor.new()
-    #   )
+    puts "create Nachricht angekommen #{payload.inspect}"
+    Lanalytics::Processing.instance.process_data_for(message.delivery_info[:routing_key], payload, processing_opts(message))
   end
 
   def update
-
-    puts "Nachricht angekommen #{payload.inspect}"
-    Lanalytics::Processing.instance.process_data_for(message.delivery_info[:routing_key], payload, message)
-
-
-    #Lanalytics::Processing.new(
-    #  Lanalytics.Processor::Neo4jProcessor.new(),
-    #
-    #  Lanalytics.Processor::Neo4jProcessor.new()
-    #  Lanalytics.Processor::Neo4jProcessor.new()
-    #  )
+    puts "update Nachricht angekommen #{payload.inspect}"
+    Lanalytics::Processing.instance.process_data_for(message.delivery_info[:routing_key], payload, processing_opts(message))
   end
 
   def destroy
-    puts "Nachricht angekommen #{payload.inspect}"
+    puts "Destroy Nachricht angekommen #{payload.inspect}"
+    Lanalytics::Processing.instance.process_data_for(message.delivery_info[:routing_key], payload, processing_opts(message))
+  end
+
+  def processing_opts(message)
+    return {
+      import_action: "#{/xikolo\..*\..*\.(?<action>create|update|destroy)/.match(message.delivery_info[:routing_key])[:action]}".to_sym.upcase,
+      rabbit_message: message
+    }
   end
 end
