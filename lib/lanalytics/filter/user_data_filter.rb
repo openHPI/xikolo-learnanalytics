1dcd0bed3d982f3e454a7f09857cd730b6ff9b78
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2014-10-23 21:05:57 +0200

    Implemented test for and extended user_data_filter.rb

--- lib/lanalytics/filter/user_data_filter.rb
+++ lib/lanalytics/filter/user_data_filter.rb
@@ -3,7 +3,21 @@ module Lanalytics
     
     class UserDataFilter < Lanalytics::Filter::DataFilter
       def filter(original_resource_as_hash, processed_resources, opts = nil)
-        processed_resources << Lanalytics::Model::StmtResource.new(:USER, original_resource_as_hash[:id], original_resource_as_hash.except(:id, :emails_url, :email_url, :self_url, :blurb_url, :image_id, :legacy_id))    
+
+        uninteresting_properties = [:id, :emails_url, :email_url, :self_url, :blurb_url, :legacy_id, :fields]
+
+        resource_properties = original_resource_as_hash.except(*uninteresting_properties)
+        
+        if original_resource_as_hash.key?(:fields) and not original_resource_as_hash[:fields].nil?
+          original_resource_as_hash[:fields].each do | field_hash |
+            field_hash = field_hash.with_indifferent_access
+            field_name = field_hash[:name]
+            field_value = field_hash[:values].first
+            resource_properties[field_name] = field_value unless field_value.nil? or field_value.blank?
+          end
+        end
+
+        processed_resources << Lanalytics::Model::StmtResource.new(:USER, original_resource_as_hash[:id], resource_properties)    
       end
     end
   
--- spec/factories/amqp_event_data_factory.rb
+++ spec/factories/amqp_event_data_factory.rb
@@ -63,4 +63,25 @@ FactoryGirl.define do
     affiliated false
   end
 
+factory :amqp_user_with_fields, class: Hash do
+    id "00000001-3100-4444-9999-000000000001"
+    full_name "Kevin Cool Jr."
+    admin false
+    language "en"
+    born_at "1985-04-24T00:00:00.000Z"
+    created_at "2014-10-20T19:56:31.268Z"
+    fields [
+
+  {"id"=>"178fcc30-b5e8-41f7-bdb8-946c1e908cc8", "name"=>"affiliation", "title"=>{"en"=>"Affiliation"}, "type"=>"CustomTextField", "available_values"=>nil, "default_values"=>[""], "required"=>false, "values"=>["Hasso Plattner Institute"]},
+  
+  {"id"=>"a58626e4-9461-4e0c-bfd6-3b0c674db7db", "name"=>"country", "title"=>{"en"=>"Country"}, "type"=>"CustomSelectField", "available_values"=>["not_set", "ad", "ae", "af", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "as", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bl", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr", "cs", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "eh", "er", "es", "et", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mf", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "sv", "sy", "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "um", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "ye", "yt", "za", "zm", "zw", "zz"], "default_values"=>["not_set"], "required"=>false, "values"=>[""]},
+  
+  {"id"=>"7bee1d5d-de6a-412c-a54a-0dec94763763", "name"=>"city", "title"=>{"en"=>"City"}, "type"=>"CustomTextField", "available_values"=>nil, "default_values"=>[""], "required"=>false, "values"=>["Potsdam"]},
+  
+  {"id"=>"4124ffb8-fd65-45b1-80d7-af279c32bc22", "name"=>"gender", "title"=>{"en"=>"Gender"}, "type"=>"CustomSelectField", "available_values"=>["not_set", "male", "female", "other"], "default_values"=>["not_set"], "required"=>false, "values"=>["male"]}
+]
+
+
+  end
+
 end
