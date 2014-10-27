fd9af5b91450a81887dfc547e6aef76c4744c550
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2014-10-27 10:48:57 +0100

    Included test configuration for Neo4j

--- config/initializers/neo4j_database_session.rb
+++ config/initializers/neo4j_database_session.rb
@@ -7,9 +7,7 @@ if File.exists?(NEO4J_DATABASE_CONFIG_FILE)
   # Register a default Neo4j::Session for this applilcation
   neo4j_db_type = NEO4J_DATABASE_CONFIG[:db_type].to_sym
   neo4j_db_url = NEO4J_DATABASE_CONFIG[:db_url]
-  session = Neo4j::Session.open(neo4j_db_type, neo4j_db_url)
-  # if Rails.env.test?
-  #   Rails.logger.warn "No Neo4j::Session could be created. But the test continues"
-  # else
+  session = Neo4j::Session.open(neo4j_db_type, neo4j_db_url, NEO4J_DATABASE_CONFIG.except(:db_type, :db_url))
+
   raise 'No Neo4j::Session could be created. Plz have a look at the configuration ...' unless session
 end
