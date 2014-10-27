0d0b3699a4ac35437aa790b44602fd10f260776a
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2014-10-27 10:53:55 +0100

    Completed the Processing Refactoring

--- app/consumer/lanalytics_resource_consumer.rb
+++ app/consumer/lanalytics_resource_consumer.rb
@@ -2,23 +2,23 @@ class LanalyticsResourceConsumer < Msgr::Consumer
 
   def create
     puts "create Nachricht angekommen #{payload.inspect}"
-    Lanalytics::Processing.instance.process_data_for(message.delivery_info[:routing_key], payload, processing_opts(message))
+    Lanalytics::Processing::AmqpProcessingManager.instance.process_data_for(message.delivery_info[:routing_key], payload, processing_opts(message))
   end
 
   def update
     puts "update Nachricht angekommen #{payload.inspect}"
-    Lanalytics::Processing.instance.process_data_for(message.delivery_info[:routing_key], payload, processing_opts(message))
+    Lanalytics::Processing::AmqpProcessingManager.instance.process_data_for(message.delivery_info[:routing_key], payload, processing_opts(message))
   end
 
   def destroy
     puts "Destroy Nachricht angekommen #{payload.inspect}"
-    Lanalytics::Processing.instance.process_data_for(message.delivery_info[:routing_key], payload, processing_opts(message))
+    Lanalytics::Processing::AmqpProcessingManager.instance.process_data_for(message.delivery_info[:routing_key], payload, processing_opts(message))
   end
 
   def processing_opts(message)
     return {
-      import_action: "#{/xikolo\..*\..*\.(?<action>create|update|destroy)/.match(message.delivery_info[:routing_key])[:action]}".to_sym.upcase,
-      rabbit_message: message
+      # processing_action: "#{/xikolo\..*\..*\.(?<action>create|update|destroy)/.match(message.delivery_info[:routing_key])[:action]}".to_sym.upcase,
+      amqp_message: message
     }
   end
 end
