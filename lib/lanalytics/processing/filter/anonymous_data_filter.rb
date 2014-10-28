a766907043d0448b41f3a280f65a83cf8216bffc
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2014-10-28 21:41:23 +0100

    Extended anonymous filter to understand raltionships as well

--- lib/lanalytics/processing/filter/anonymous_data_filter.rb
+++ lib/lanalytics/processing/filter/anonymous_data_filter.rb
@@ -6,17 +6,20 @@ module Lanalytics
 
         def filter(original_resource_as_hash, processed_resources, opts = nil)
 
-          processed_resources.map! do | processed_resource |
-            next unless processed_resource.is_a? Lanalytics::Model::StmtResource
-            processed_resource.properties.delete_if { |key, value| symbol_anonymous?(key) }
-            processed_resource
+          processed_resources.map! do | processed_entity |
+            if processed_entity.is_a? Lanalytics::Model::StmtResource or processed_entity.is_a? Lanalytics::Model::ResourceRelationship
+              processed_entity.properties.delete_if { |key, value| symbol_anonymous?(key) }
+              processed_entity
+            else
+              processed_entity
+            end
           end
         end
         alias_method :process, :filter
 
         private
         def symbol_anonymous?(symbol)
-          %w(email name password).each do | dangerous_keyword |
+          %w(mail email name password).each do | dangerous_keyword |
             return true if symbol.to_s.include?(dangerous_keyword)
           end
           return false
