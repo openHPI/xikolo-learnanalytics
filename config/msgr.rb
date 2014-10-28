d74e3f40892ff3dda0ee49e34b3f6c2bef39ab85
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2014-10-28 10:16:01 +0100

    QuizSubmission tracked

--- config/msgr.rb
+++ config/msgr.rb
@@ -1,4 +1,4 @@
-# ------------------- User Entities -------------------
+# ------------------- User Domain Entities -------------------
 route 'xikolo.account.user.create', to: 'Lanalytics#create'
 route 'xikolo.account.user.update', to: 'Lanalytics#update'
 route 'xikolo.account.user.destroy', to: 'Lanalytics#destroy'
@@ -6,7 +6,7 @@ route 'xikolo.account.user.destroy', to: 'Lanalytics#destroy'
 route 'xikolo.account.session.create', to: 'Lanalytics#create'
 
 
-# ------------------- Course Entities -------------------
+# ------------------- Course Domain Entities -------------------
 route 'xikolo.course.course.create', to: 'Lanalytics#create'
 route 'xikolo.course.course.update', to: 'Lanalytics#update'
 route 'xikolo.course.course.destroy', to: 'Lanalytics#destroy'
@@ -18,7 +18,7 @@ route 'xikolo.course.item.destroy', to: 'Lanalytics#destroy'
 route 'xikolo.course.visit.create', to:  'Lanalytics#create'
 
 
-# ------------------- Learning Room Entities -------------------
+# ------------------- Learning Room Domain Entities -------------------
 route 'xikolo.learning_room.learning_room.create', to: 'Lanalytics#create'
 route 'xikolo.learning_room.learning_room.update', to: 'Lanalytics#update'
 route 'xikolo.learning_room.learning_room.destroy', to: 'Lanalytics#destroy'
@@ -26,6 +26,10 @@ route 'xikolo.learning_room.learning_room.destroy', to: 'Lanalytics#destroy'
 route 'xikolo.learning_room.membership.create', to: 'Lanalytics#create'
 route 'xikolo.learning_room.membership.update', to: 'Lanalytics#update'
 route 'xikolo.learning_room.membership.destroy', to: 'Lanalytics#destroy'
+
+
+# ------------------- Submissions Domain Entities -------------------
+route 'xikolo.submission.submission.create', to: 'Lanalytics#create'
 
 
 # ------------------- Web Events -------------------
