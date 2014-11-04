7971b84f1237ecc786e5b4148a32e834d1a54858
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2014-11-04 19:31:40 +0100

    Little refactoring

--- lib/lanalytics/model/stmt_component.rb
+++ lib/lanalytics/model/stmt_component.rb
@@ -1,29 +1,33 @@
 require 'json'
 
-class Lanalytics::Model::StmtComponent
-  attr_reader :type
+module Lanalytics
+  module Model
+    class StmtComponent
+      attr_reader :type
 
-  def initialize(type)
+      def initialize(type)
 
-    raise ArgumentError.new("'type' argument cannot be nil") unless type.is_a? String or type.is_a? Symbol
-    raise ArgumentError.new("'type' argument cannot be empty") if type.is_a? String and type.empty?
-    @type = type.to_sym.upcase
-  end
+        raise ArgumentError.new("'type' argument cannot be nil") unless type.is_a? String or type.is_a? Symbol
+        raise ArgumentError.new("'type' argument cannot be empty") if type.is_a? String and type.empty?
+        @type = type.to_sym.upcase
+      end
 
-  # JSON Serialization (Marshalling)
-  def to_json(*a)
-    {
-        :json_class => self.class.name,
-        :data => self.as_json
-    }.to_json(*a)
-  end
+      # JSON Serialization (Marshalling)
+      def to_json(*a)
+        {
+            :json_class => self.class.name,
+            :data => self.as_json
+        }.to_json(*a)
+      end
 
-  def ==(other)
-    unless other.class == self.class
-      return false
+      def ==(other)
+        unless other.class == self.class
+          return false
+        end
+        return @type == other.type
+      end
+      alias_method :eql?, :==
+
     end
-    return @type == other.type
   end
-  alias_method :eql?, :==
-
 end
