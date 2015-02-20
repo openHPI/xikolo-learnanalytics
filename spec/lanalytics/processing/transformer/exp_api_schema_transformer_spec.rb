62cd3d00fcbb6d03db806575cea865b0abd46ebe
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2015-02-20 12:06:54 +0100

    Add spec

---
+++ spec/lanalytics/processing/transformer/exp_api_schema_transformer_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+shared_examples 'an experience statement' do
+  its(:entity_key) { is_expected.to eq :EXP_STATEMENT }
+  it 'has the correct attributes' do
+    expect(subject.attributes.map(&:name)).to match_array(%i(user verb resource timestamp in_context))
+  end
+end
+
+describe Lanalytics::Processing::Transformer::ExpApiSchemaTransformer do
+  let(:transformer) { described_class.new }
+  let(:load_commands) { [] }
+  let(:transform_method) { transformer.method("transform_#{type}_punit_to_create_load_commands") }
+  subject do
+    transform_method.call(processing_unit, load_commands)
+    load_commands.first.entity
+  end
+
+  describe 'ask question' do
+    let(:type) { 'question' }
+    let(:processing_unit) do
+      {
+        id: '00000001-3500-4444-9999-000000000001',
+        title: 'Title',
+        text: 'Text',
+        user_id: SecureRandom.uuid,
+        course_id: SecureRandom.uuid,
+        technical: false
+      }
+    end
+
+    it_behaves_like 'an experience statement'
+    it 'has the correct verb' do
+      expect(subject[:verb].value).to eq :ASKED_QUESTION
+    end
+  end
+
+  describe 'answer_question' do
+    let(:type) { 'answer' }
+    let(:processing_unit) do
+      {
+        id: '00000002-3500-4444-9999-000000000001',
+        user_id: SecureRandom.uuid,
+        course_id: SecureRandom.uuid,
+        technical: false
+      }
+    end
+
+    it_behaves_like 'an experience statement'
+    it 'has the correct verb' do
+      expect(subject[:verb].value).to eq :ANSWERED_QUESTION
+    end
+  end
+
+  describe 'comment' do
+    let(:type) { 'comment' }
+    let(:processing_unit) do
+      {
+        id: '00000003-3500-4444-9999-000000000001',
+        text: 'Text',
+        user_id: SecureRandom.uuid,
+        course_id: SecureRandom.uuid,
+        technical: false
+      }
+    end
+
+    it_behaves_like 'an experience statement'
+    it 'has the correct verb' do
+      expect(subject[:verb].value).to eq :COMMENTED
+    end
+  end
+end
