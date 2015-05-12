8192c8e9a4952eab562bc845be28a15c361411b2
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2015-05-12 12:20:36 +0200

    Add course activity metric

---
+++ lib/lanalytics/metric/course_activity.rb
@@ -0,0 +1,10 @@
+module Lanalytics
+  module Metric
+    class  CourseActivity < CombinedMetric
+      def self.dependent_metrics
+        [{class: PinboardActivity, weight: 0.5},
+         {class: VisitCount}]
+      end
+    end
+  end
+end
---
+++ spec/lanalytics/metric/course_activity_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Lanalytics::Metric::CourseActivity do
+  let(:user_id) { SecureRandom.uuid }
+  let(:course_id) { SecureRandom.uuid }
+  let(:start_time) { 2.weeks.ago.to_s }
+  let(:end_time) { Time.now.to_s }
+
+  describe '#query' do
+    subject { described_class.query user_id, course_id, start_time, end_time }
+
+    it 'sums the two metrics' do
+      expect(Lanalytics::Metric::PinboardActivity).to receive(:query).and_return(count: 50)
+      expect(Lanalytics::Metric::VisitCount).to receive(:query).and_return(count: 10)
+      expect(subject[:count]).to eq 35
+    end
+  end
+end
