6b94374bf7e08317e7551c5f46edcec1906e27e5
Kai Rollmann <kai.rollmann@googlemail.com>
2015-09-28 11:47:39 +0200

    Lint and refactor pipeline.rb

--- lib/lanalytics/processing/pipeline.rb
+++ lib/lanalytics/processing/pipeline.rb
@@ -5,54 +5,81 @@ module Lanalytics
 
       def initialize(name, schema, processing_action, extractors = [], transformers = [], loaders = [], &block)
         # Check mandatory fields
-        raise ArgumentError.new "'#{name}' cannot be nil or empty" if name.nil? or name.empty? 
-        raise ArgumentError.new "'#{schema}' cannot be nil" if schema.nil? or schema.empty? 
-        raise ArgumentError.new ("'#{processing_action}' is not accepted; only 'ProcessingAction::{CREATE, UPDATE, DESTROY, UNDEFINED}' are accepted") if processing_action.nil? or not Lanalytics::Processing::ProcessingAction.valid(processing_action)
+        if name.nil? || name.empty?
+          fail ArgumentError, "'#{name}' cannot be nil or empty"
+        end
+
+        if schema.nil? || schema.empty?
+          fail ArgumentError, "'#{schema}' cannot be nil"
+        end
+
+        if processing_action.nil? || !Lanalytics::Processing::ProcessingAction.valid(processing_action)
+          fail ArgumentError, "'#{processing_action}' is not accepted; only 'ProcessingAction::{CREATE, UPDATE, DESTROY, UNDEFINED}' are accepted"
+        end
 
         extractors ||= []
-        extractors.each_with_index do | extract_step, i |
-          raise ArgumentError.new("Element #{i} in extractors is a '#{extract_step.class.name}', but needs to be an 'Lanalytics::Processing::Extractor::ExtractStep'") unless extract_step.is_a?(Lanalytics::Processing::Extractor::ExtractStep)
-        end 
+        extractors.each_with_index do |extract_step, i|
+          unless extract_step.is_a?(Lanalytics::Processing::Extractor::ExtractStep)
+            fail ArgumentError, "Element #{i} in extractors is a '#{extract_step.class.name}', but needs to be a 'Lanalytics::Processing::Extractor::ExtractStep'"
+          end
+        end
 
         transformers ||= []
-        transformers.each_with_index do | transform_step, i |
-          raise ArgumentError.new("Element #{i} in transformers is a '#{transform_step.class.name}', but needs to be an 'Lanalytics::Processing::Transformer::TransformStep'") unless transform_step.is_a?(Lanalytics::Processing::Transformer::TransformStep)
+        transformers.each_with_index do |transform_step, i|
+          unless transform_step.is_a?(Lanalytics::Processing::Transformer::TransformStep)
+            fail ArgumentError, "Element #{i} in transformers is a '#{transform_step.class.name}', but needs to be a 'Lanalytics::Processing::Transformer::TransformStep'"
+          end
         end
 
         loaders ||= []
-        loaders.each_with_index do | load_step, i |
-          raise ArgumentError.new("Element #{i} in loaders is a '#{load_step.class.name}', but needs to be an 'Lanalytics::Processing::Loader::LoadStep'") unless load_step.is_a?(Lanalytics::Processing::Loader::LoadStep)
+        loaders.each_with_index do |load_step, i|
+          unless load_step.is_a?(Lanalytics::Processing::Loader::LoadStep)
+            fail ArgumentError, "Element #{i} in loaders is a '#{load_step.class.name}', but needs to be a 'Lanalytics::Processing::Loader::LoadStep'"
+          end
         end
 
-        @name, @schema, @processing_action, @extractors, @transformers, @loaders = name, schema.to_sym, processing_action, extractors, transformers, loaders
-        
+        @name              = name
+        @schema            = schema.to_sym
+        @processing_action = processing_action
+        @extractors        = extractors
+        @transformers      = transformers
+        @loaders           = loaders
+
         instance_eval(&block) if block_given?
 
         # TODO:: Check for validity and type
       end
-      
+
       # These methods are used inside the block when block is initialized ...
       def extractor(extractor)
-        raise ArgumentError.new('Needs to be of type \'ExtractStep\'!!') unless extractor and extractor.is_a?(Lanalytics::Processing::Extractor::ExtractStep)
+        unless extractor && extractor.is_a?(Lanalytics::Processing::Extractor::ExtractStep)
+          fail ArgumentError, 'Needs to be of type \'ExtractStep\'.'
+        end
+
         @extractors << extractor
       end
 
       def transformer(transformer)
-        raise ArgumentError.new('Needs to be of type \'TransformerStep\'!!') unless transformer and transformer.is_a?(Lanalytics::Processing::Transformer::TransformStep)
+        unless transformer && transformer.is_a?(Lanalytics::Processing::Transformer::TransformStep)
+          fail ArgumentError, 'Needs to be of type \'TransformerStep\'.'
+        end
+
         @transformers << transformer
       end
 
       def loader(loader)
-        raise ArgumentError.new('Needs to be of type \'LoadStep\'!!') unless loader and loader.is_a?(Lanalytics::Processing::Loader::LoadStep)
+        unless loader && loader.is_a?(Lanalytics::Processing::Loader::LoadStep)
+          fail ArgumentError, 'Needs to be of type \'LoadStep\'.'
+        end
+
         @loaders << loader
       end
 
       def full_name
-        return "#{schema}::#{name}"
+        "#{schema}::#{name}"
       end
 
       def process(original_event, processing_opts = {})
-
         processing_opts ||= {}
 
         unless original_event
@@ -65,14 +92,14 @@ module Lanalytics
         pipeline_ctx = PipelineContext.new(self, processing_opts)
 
         processing_units = []
-        @extractors.each do | extractor |
+        @extractors.each do |extractor|
           extractor.extract(original_event, processing_units, pipeline_ctx)
         end
 
-        # Take care of 'processing_units' and 'load_commands' because these arrays are mutable and are modified in the transformation steps
+        # Take care of 'processing_units' and 'load_commands'
+        # because these arrays are mutable and are modified in the transformation steps
         load_commands = []
-        @transformers.each do | transformer |
-          
+        @transformers.each do |transformer|
           Rails.logger.debug(%Q{
             Transformer #{transformer.class.name} received the following processing units:
             #{processing_units.inspect}
@@ -82,11 +109,11 @@ module Lanalytics
 
           Rails.logger.debug(%Q{
             Transformer #{transformer.class.name} transformed to the following load commands:
-            #{load_commands.inspect} 
+            #{load_commands.inspect}
           })
         end
 
-        @loaders.each do | loader |
+        @loaders.each do |loader|
           loader.load(original_event, load_commands, pipeline_ctx)
         end
       end
@@ -103,11 +130,11 @@ module Lanalytics
       end
 
       def name
-        return @pipeline.name
+        @pipeline.name
       end
 
       def processing_action
-        return @pipeline.processing_action
+        @pipeline.processing_action
       end
     end
   end
