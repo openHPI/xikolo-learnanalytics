3a03a455b6fe4cc08055a2509153b6a8a7c7cffc
Kai Rollmann <kai.rollmann@googlemail.com>
2015-09-29 00:41:20 +0200

    Lint datasource manager

--- lib/lanalytics/processing/datasource_manager.rb
+++ lib/lanalytics/processing/datasource_manager.rb
@@ -3,6 +3,8 @@ module Lanalytics
     class DatasourceManager
       include Singleton
 
+      attr_reader :datasources
+
       def initialize
         @datasources = {}
       end
@@ -12,34 +14,33 @@ module Lanalytics
       end
 
       def self.add_datasource(datasource)
-        self.instance.add_datasource(datasource)
-      end
-
-      def get_datasources
-        return @datasources
+        instance.add_datasource(datasource)
       end
 
       def self.get_datasource(datasource_key)
-        self.instance.get_datasources[datasource_key]
+        instance.datasources[datasource_key]
       end
 
       def self.datasource_exists?(datasource_key)
-        self.instance.get_datasources.has_key?(datasource_key)
+        instance.datasources.key?(datasource_key)
       end
 
       def self.new_datasource
-
         return unless block_given?
 
         datasource = yield
 
-        raise ArgumentError.new "The block has to return a Datasource ..." unless datasource.is_a?(Lanalytics::Processing::Datasources::Datasource)
-        raise ArgumentError.new "The returned Datasource has to contain a key" unless datasource.key
+        unless datasource.is_a?(Lanalytics::Processing::Datasources::Datasource)
+          fail ArgumentError, 'The block has to return a Datasource ...'
+        end
 
-        self.instance.add_datasource(datasource)
+        unless datasource.key
+          fail ArgumentError, 'The returned Datasource has to contain a key'
+        end
 
-        return datasource 
+        instance.add_datasource(datasource)
 
+        datasource
       end
 
     end
