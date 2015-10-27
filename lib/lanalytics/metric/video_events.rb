5029c46ae27709dd12b512e7d528495777bc192c
Jan Renz <jan.renz@hpi.uni-potsdam.de>
2015-10-27 11:42:36 +0100

    Added fullscreen off

--- lib/lanalytics/metric/video_events.rb
+++ lib/lanalytics/metric/video_events.rb
@@ -2,12 +2,13 @@ module Lanalytics
   module Metric
     class VideoEvents < ExpApiMetric
       def self.query(user_id, course_id, start_time, end_time, ressource_id)
-        pause = get_data('VIDEO_PAUSE', ressource_id)
-        play = get_data('VIDEO_PLAY', ressource_id)
-        change_speed = get_data('CHANGE_SPEED', ressource_id)
-        stop = get_data('VIDEO_STOP', ressource_id)
-        seek = get_data('VIDEO_SEEK', ressource_id)
-        fullscreen = get_data('VIDEO_FULLSCREEN', ressource_id)
+        pause = get_data('VIDEO_PAUSE', ressource_id, nil)
+        play = get_data('VIDEO_PLAY', ressource_id, nil)
+        change_speed = get_data('VIDEO_CHANGE_SPEED', ressource_id, nil)
+        stop = get_data('VIDEO_STOP', ressource_id, nil)
+        seek = get_data('VIDEO_SEEK', ressource_id, nil)
+        fullscreen = get_data('VIDEO_FULLSCREEN', ressource_id,  {match_phrase: { 'in_context.new_state' => 'fullscreen'}})
+        fullscreen_off = get_data('VIDEO_FULLSCREEN', ressource_id,  {match_phrase: { 'in_context.new_state' => 'player'}})
         result = {}
 
         pause.each do |item|
@@ -36,20 +37,26 @@ module Lanalytics
           result[item['key']]['fullscreen'] = item['doc_count']
           result[item['key']]['total'] += item['doc_count']
         end
+        fullscreen_off.each do |item|
+          result[item['key']]['fullscreen'] = item['doc_count']
+          result[item['key']]['total'] += item['doc_count']
+        end
         result
       end
 
 
-      def self.get_data verb, ressource_id
+      def self.get_data verb, ressource_id, add_filter = nil
+        conditions = [
+            {match_phrase: { 'resource.resource_uuid' => ressource_id}},
+            {match: {verb: verb}}
+        ]
+        conditions << add_filter  if add_filter.present?
         result = datasource.exec do |client|
           client.search index: datasource.index, body: {
                                                    size: 0,
                                                    query: {
                                                        bool: {
-                                                           must: [
-                                                               {match_phrase: { 'resource.resource_uuid' => ressource_id}},
-                                                               {match: {verb: verb}}
-                                                           ]
+                                                           must: conditions
                                                        }
                                                    },
                                                    aggs: {
