0f441e0d871ec235dd1ce64cbd4b685a6d6c8cba
Kai Rollmann <kai.rollmann@googlemail.com>
2015-11-03 16:06:53 +0100

    Convert json keys to underscore for native DB

---
+++ lib/lanalytics/helper/hash_helper.rb
@@ -0,0 +1,17 @@
+module Lanalytics::Helper::HashHelper
+  # http://stackoverflow.com/questions/8706930/converting-nested-hash-keys-from-camelcase-to-snake-case-in-ruby
+  def underscore_key(k)
+    k.to_s.underscore.to_sym
+  end
+
+  def hash_keys_to_underscore(value)
+    case value
+      when Array
+        value.map {|v| hash_keys_to_underscore(v) }
+      when Hash
+        Hash[value.map {|k, v| [underscore_key(k), hash_keys_to_underscore(v)] }]
+      else
+        value
+    end
+  end
+end
