0e9e235f49379a3b1b5d9da813830d9ffdbc78bd
Jan Renz <jan.renz@gmail.com>
2015-12-02 18:12:13 +0100

    Fix Active user count

--- lib/lanalytics/metric/active_user_count.rb
+++ lib/lanalytics/metric/active_user_count.rb
@@ -2,7 +2,7 @@ module Lanalytics
   module Metric
     class ActiveUserCount < ExpApiMetric
       def self.query(user_id, course_id, start_time, end_time, resource_id)
-        #default 15 min
+        #default 30 min
         start_time = start_time.present? ? DateTime.parse(start_time) : (DateTime.now - 30.minutes)
         end_time = end_time.present? ? DateTime.parse(end_time) : (DateTime.now)
         result = datasource.exec do |client|
@@ -23,14 +23,14 @@ module Lanalytics
              aggs: {
                  distinct_user_count: {
                      cardinality: {
-                         field: 'user.resource_uuid"'
+                         field: 'user.resource_uuid'
                      }
                  }
              }
          }
         end
 
-        result['hits']['total']
+        result['aggregations']['distinct_user_count']['value']
       end
 
     end
