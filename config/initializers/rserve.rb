937957ef5003448f6be850a42312b58567d3e94f
Kai Rollmann <kai.rollmann@googlemail.com>
2016-02-25 14:06:31 +0100

    Hotfix rserve config

--- config/initializers/rserve.rb
+++ config/initializers/rserve.rb
@@ -1,1 +1,1 @@
-Lanalytics::RSERVE_CONFIG = YAML.load_file("#{Rails.root}/config/rserve.yml")[Rails.env]
+Lanalytics::RSERVE_CONFIG = YAML.load_file("#{Rails.root}/config/rserve.yml")[Rails.env].with_indifferent_access
