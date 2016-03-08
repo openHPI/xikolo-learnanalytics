affd610725b2ba30f9aebf4f88bc8ac0ac71526b
Virginia Weidhaas <virginia.weidhaas@gmail.com>
2016-03-08 09:23:42 +0100

    Update Formatting, remove lines

--- lib/lanalytics/metric/quiz_performance.rb
+++ lib/lanalytics/metric/quiz_performance.rb
@@ -4,15 +4,15 @@ module Lanalytics
 
       def self.query(user_id, course_id, start_date = nil, end_date = nil, resource_id, page, per_page)
         query = {
-            filtered: {
-              filter: {
-                bool: {
-                  must: [
-            {match: {verb: 'SUBMITTED_QUIZ'}}
-        ] + (all_filters(course_id, user_id))
-        }
-        }
-        }
+          filtered: {
+            filter: {
+              bool: {
+                must: [
+                  {match: {verb: 'SUBMITTED_QUIZ'}}
+                ] + (all_filters(course_id, user_id))
+              }
+            }
+          }
         }
         if resource_id.present?
           query[:filtered][:filter][:bool][:must] << {match_phrase: {"in_context.item_id" => resource_id}}
@@ -21,7 +21,7 @@ module Lanalytics
         end
         quiz_statements = datasource.exec do |client|
           client.search index: datasource.index, body: {
-            size: 20,
+            size: 0,
             query: query,
             aggs: {
               sum_points: {
@@ -35,21 +35,20 @@ module Lanalytics
               }
             }
           }
-
         end
         query[:filtered][:filter][:bool][:must] << {match_phrase: {"in_context.attempt" => 1}}
         quiz_statements_first_attempt = datasource.exec do |client|
           client.search index: datasource.index, body: {
-              size: 20,
-              query: query,
-              aggs: {
+            size: 0,
+            query: query,
+            aggs: {
               sum_points: {
-              sum: {field: "in_context.points"}
-          },
+                sum: {field: "in_context.points"}
+              },
               sum_max_points: {
-              sum: {field: "in_context.max_points"}
-          },
-          }
+                sum: {field: "in_context.max_points"}
+              },
+            }
           }
         end
         first_attempt_avg = 0
@@ -59,13 +58,9 @@ module Lanalytics
         first_attempt_avg = quiz_statements_first_attempt["aggregations"]["sum_points"]["value"] / quiz_statements_first_attempt["aggregations"]["sum_max_points"]["value"].to_f * 100 unless quiz_statements_first_attempt["aggregations"]["sum_max_points"]["value"] == 0
         return {total: quiz_statements["hits"]["total"],
             average_points_percentage: avg,
-            #quiz_statements: quiz_statements,
             avg_attempts:  quiz_statements["aggregations"]["avg_attempts"]["value"] ||= 0,
-            #first_attempts: quiz_statements_first_attempt,
             average_points_percentage_first_attempt: first_attempt_avg
         }
-
-
       end
     end
   end
