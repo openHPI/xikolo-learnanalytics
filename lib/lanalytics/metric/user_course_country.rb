e87fefba39f796a53f0f4e4a75d80f0a26bc3cda
Jan Renz <jan.renz@hpi.de>
2016-04-14 14:52:09 +0200

    Make ressource id optional so we can fetch top country per user

--- lib/lanalytics/metric/user_course_country.rb
+++ lib/lanalytics/metric/user_course_country.rb
@@ -2,24 +2,27 @@ module Lanalytics
   module Metric
     class UserCourseCountry < ExpApiMetric
       def self.query(user_id, course_id, start_time, end_time, resource_id, page, per_page)
+        conditions = []
+         if course_id.present?
+          conditions << {
+            match: {
+              "in_context.course_id" => course_id
+            }
+          }
+         end
+        if resource_id.present?
+          conditions << {
+              match: {
+                  "user.resource_uuid" => resource_id
+              }
+          }
+        end
         result = datasource.exec do |client|
           client.search index: datasource.index, body: {
               size: 0,
               query: {
                   bool: {
-                      must: [
-                          {
-                              match: {
-                                  "in_context.course_id" => course_id
-                              }
-                          },
-                          {
-                              match: {
-                                  "user.resource_uuid" => resource_id
-                              }
-                          }
-
-                      ]
+                      must: conditions
                   }
               },
               aggregations: {
