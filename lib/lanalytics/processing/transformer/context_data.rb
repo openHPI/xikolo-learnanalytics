eeddfec154dccf0007deb47d08095225c315adb2
Tobias Rohloff <tobias.rohloff@student.hpi.uni-potsdam.de>
2016-06-20 10:53:09 +0200

    Rescue browser gem exceptions.

--- lib/lanalytics/processing/transformer/context_data.rb
+++ lib/lanalytics/processing/transformer/context_data.rb
@@ -3,6 +3,7 @@ module Lanalytics
     module Transformer
 
       class ContextData < TransformStep
+
         def transform(original_event, processing_units, load_commands, pipeline_ctx)
           processing_units.each do |processing_unit|
             next if processing_unit[:in_context].nil? || processing_unit[:in_context][:user_agent].nil?
@@ -11,16 +12,25 @@ module Lanalytics
 
             browser = Browser.new(user_agent)
 
-            processing_unit[:in_context][:platform]         = browser.platform.name
-            processing_unit[:in_context][:platform_version] = browser.platform.version
-            processing_unit[:in_context][:runtime]          = browser.name
-            processing_unit[:in_context][:runtime_version]  = browser.version
-            processing_unit[:in_context][:device]           = browser.device.name
+            processing_unit[:in_context][:platform]         = safe_read { browser.platform.name }     || "Unknown"
+            processing_unit[:in_context][:platform_version] = safe_read { browser.platform.version }  || "0"
+            processing_unit[:in_context][:runtime]          = safe_read { browser.name }              || "Unknown"
+            processing_unit[:in_context][:runtime_version]  = safe_read { browser.version }           || "0"
+            processing_unit[:in_context][:device]           = safe_read { browser.device.name }       || "Unknown"
 
             # merge platfrom 'iOS (iPhone)', 'iOS (iPad)' and 'iOS (iPod)' to 'iOS'
-            processing_unit[:in_context][:platform]         = "iOS" if browser.platform.name.start_with? "iOS"
+            processing_unit[:in_context][:platform]         = "iOS" if processing_unit[:in_context][:platform].start_with? "iOS"
           end
         end
+
+        private
+
+        def safe_read(&block)
+            yield
+          rescue
+            nil
+        end
+
       end
 
     end
