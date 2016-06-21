1277a8c69812c53f11d80d4be609db2487879770
Tobias Rohloff <tobias.rohloff@student.hpi.uni-potsdam.de>
2016-06-21 16:46:01 +0200

    Add a collection metric for reportings.

--- app/controllers/api/query_controller.rb
+++ app/controllers/api/query_controller.rb
@@ -70,7 +70,7 @@ class Api::QueryController < ApplicationController
        ItemDiscovery VideoDiscovery QuizDiscovery
        VideoPlayerActivity DownloadActivity CoursePerformance
        QuizPerformance UngradedQuizPerformance GradedQuizPerformance
-       MainQuizPerformance BonusQuizPerformance)
+       MainQuizPerformance BonusQuizPerformance CourseExportCollection)
   end
 
   def query_params
