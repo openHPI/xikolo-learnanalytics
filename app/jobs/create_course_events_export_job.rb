c0d007a1e6848bff44871c9bb6af308a49100666
Tobias Rohloff <tobias.rohloff@student.hpi.uni-potsdam.de>
2016-09-29 13:51:57 +0200

    Fix and refactor course events export.

--- app/jobs/create_course_events_export_job.rb
+++ app/jobs/create_course_events_export_job.rb
@@ -5,7 +5,6 @@ class CreateCourseEventsExportJob < CreateExportJob
     job = find_and_save_job (job_id)
 
     begin
-      #course_id = 'c1556425-5449-4b05-97b3-42b38a39f6c5' #manual overwrite
       course = Xikolo::Course::Course.find(course_id)
       Acfs.run
       job.annotation = course.course_code.to_s
@@ -30,13 +29,14 @@ class CreateCourseEventsExportJob < CreateExportJob
     file = Tempfile.open(job_id.to_s, get_tempdir)
     excel_tmp_file =  Tempfile.new('excel_course_export')
     headers = []
-    courseevent_info = []
+    course_event_info = []
     @filepath = File.absolute_path(file)
 
     course = Xikolo::Course::Course.find(course_id)
     items = ActiveSupport::HashWithIndifferentAccess.new
     sections = ActiveSupport::HashWithIndifferentAccess.new
-    #we may need to include deleted if there is usage data for those
+
+    # we may need to include deleted if there is usage data for those
     Xikolo::Course::Section.each_item(course_id: course_id) do |section|
       sections[section.id] = section
     end
@@ -46,17 +46,16 @@ class CreateCourseEventsExportJob < CreateExportJob
     Acfs.run
 
     CSV.open(@filepath, 'wb') do |csv|
-      headers += ['Course ID', 'Verb', 'User', 'Timestamp', 'Resource', 'Action', 'Typ', 'Title', 'Section' ]
+      headers += ['Course ID', 'User', 'Verb', 'Resource', 'Timestamp', 'Context', 'Type', 'Title', 'Section' ]
       csv << headers
       Sidekiq.logger.debug 'Writing export to '+ @filepath + " \n" + 'with headers ' + headers.to_s
-      i = 0
       if course.start_date.present? and course.end_date.present?
-        logger.debug 'get data'
-        get_all course, csv, courseevent_info, items, sections
+        get_all course, csv, course_event_info, items, sections
       end
     end
     Acfs.run
-    excel_file = excel_attachment('CourseEventsExport', excel_tmp_file, headers, courseevent_info)
+
+    excel_file = excel_attachment('CourseEventsExport', excel_tmp_file, headers, course_event_info)
     return file, excel_file
   ensure
     file.close
@@ -70,47 +69,56 @@ class CreateCourseEventsExportJob < CreateExportJob
     job.save!
   end
 
-  def get_all(course, csv, courseevent_info, items, sections)
+  def get_all(course, csv, course_event_info, items, sections)
     page = 1
     scroll_id = nil
+
     loop do
-      paged = do_query page, course, scroll_id
+      paged = do_query(page, course, scroll_id)
+
+      Sidekiq.logger.debug "Processing data: #{paged[:data].size} items of page #{page}"
+
       paged[:data].each do |item|
-        id = item['resource']
-        if item['verb'] == 'VISITED'
+        # deprecated event
+        next if item[:verb] == 'VISITED'
+
+        if item[:verb] == 'VISITED_ITEM'
           begin
-            item['type'] = items[id].content_type
-            # this is a course item
-            item['title'] = items[id].title
-            item['section'] = sections[items[id].section_id].title
-            item['action'] = item['verb'] + ' ' + item['title'] + ' ' + item['type'] + ' ' + id #custom action name
+            id = item[:resource]
+            item[:type] = items[id].content_type
+            item[:title] = items[id].title
+            item[:section] = sections[items[id].section_id].title
           rescue
-            item['type'] = ''
-            item['title'] = ''
-            item['section'] = ''
+            item[:type] = ''
+            item[:title] = ''
+            item[:section] = ''
           end
         else
-          item['type'] = ''
-          item['title'] = ''
-          item['section'] = ''
+          item[:type] = ''
+          item[:title] = ''
+          item[:section] = ''
         end
+
         csv << item.values
-        courseevent_info << item.values
+        course_event_info << item.values
       end
+
       scroll_id = paged[:scroll_id]
-      break unless paged[:next] == true
-      page = page + 1
+      break unless paged[:next]
+      page += 1
     end
   end
 
   def do_query(page, course, scroll_id)
     Lanalytics::Metric::CourseEvents.query(
-        nil,
-        course.id,
-        course.start_date.iso8601,
-        course.end_date.iso8601,
-        page,
-        nil,
-        scroll_id)
+      nil,
+      course.id,
+      course.start_date.iso8601,
+      course.end_date.iso8601,
+      nil,
+      page,
+      nil,
+      scroll_id)
   end
+
 end
