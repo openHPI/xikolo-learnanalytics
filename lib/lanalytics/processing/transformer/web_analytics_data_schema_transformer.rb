0c8e500fe31a23ba9a03da21384156a16a34b33b
Tobias Rohloff <tobias.rohloff@student.hpi.uni-potsdam.de>
2016-11-03 17:01:39 +0100

    Fix course id and tracking id for referral tracking.

--- lib/lanalytics/processing/transformer/web_analytics_data_schema_transformer.rb
+++ lib/lanalytics/processing/transformer/web_analytics_data_schema_transformer.rb
@@ -9,11 +9,12 @@ module Lanalytics
               with_attribute :referrer, :string, punit[:referrer] if punit[:referrer].present?
               with_attribute :referrer_page, :string, punit[:referrer_page] if punit[:referrer_page].present?
               with_attribute :course_id, :uuid, punit[:course_id] if punit[:course_id].present?
+              with_attribute :course_id, :uuid, punit[:tracking_course_id] if punit[:tracking_course_id].present?
               with_attribute :user_id, :uuid, punit[:user_id] if punit[:user_id].present?
               with_attribute :user_id, :uuid, punit[:tracking_user] if punit[:tracking_user].present?
               with_attribute :timestamp, :timestamp, punit[:created_at].present? ? punit[:created_at] : DateTime.now
               with_attribute :tracking_campaign, :uuid, punit[:tracking_campaign] if punit[:tracking_campaign].present?
-              with_attribute :tracking_id, :uuid, punit[:tracking_camp] if punit[:tracking_campaign].present?
+              with_attribute :tracking_id, :uuid, punit[:tracking_id] if punit[:tracking_id].present?
               with_attribute :tracking_type, :string, punit[:tracking_type] if punit[:tracking_type].present?
               with_attribute :tracking_external_link, :string, punit[:tracking_external_link] if punit[:tracking_external_link].present?
             end
