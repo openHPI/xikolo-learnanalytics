d5928004cb8e0838f9f524d6965013b027ed9172
Tobias Rohloff <tobias.rohloff@hpi.de>
2017-01-05 15:21:19 +0100

    Refactor referral metric.

--- lib/lanalytics/metric/referrer_metric.rb
+++ lib/lanalytics/metric/referrer_metric.rb
@@ -1,6 +1,7 @@
 module Lanalytics
   module Metric
     class ReferrerMetric
+
       def self.datasource
         Lanalytics::Processing::DatasourceManager.datasource(datasource_name)
       end
@@ -9,16 +10,6 @@ module Lanalytics
         'referral'
       end
 
-      def self.all_filters(course_id = nil, user_id = nil, ressource_id = nil)
-        filters_ = course_id.nil? ? [] : [{match_phrase: {'in_context.course_id' => course_id}}]
-        filters_ += user_id.nil? ? [] : [{match_phrase: {'user.resource_uuid' => user_id}}]
-        filters_ += ressource_id.nil? ? [] : [{match_phrase: {'resource.resource_uuid' => ressource_id}}]
-        filters_ + filters
-      end
-
-      def self.filters
-        []
-      end
     end
   end
 end
