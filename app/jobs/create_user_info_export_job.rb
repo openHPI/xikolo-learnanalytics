c8f5c47b21d4078ba03ee3aa5168686a3a5d332c
Tobias Rohloff <tobias.rohloff@hpi.de>
2017-03-15 17:30:27 +0100

    Reduce memory footprint and refactorings.

--- app/jobs/create_user_info_export_job.rb
+++ app/jobs/create_user_info_export_job.rb
@@ -4,103 +4,75 @@ class CreateUserInfoExportJob < CreateExportJob
   def perform (job_id, password, user_id, course_id = nil, privacy_flag, combined_enrollment_info_flag)
     job = find_and_save_job(job_id)
     begin
-      temp_report, temp_excel_report = create_report(job_id, privacy_flag, combined_enrollment_info_flag)
+      temp_report = create_report(job_id, privacy_flag, combined_enrollment_info_flag)
       csv_name = get_tempdir.to_s + '/UserInfoExport_' + DateTime.now.strftime('%Y-%m-%d') + '.csv'
-      excel_name = get_tempdir.to_s + '/UserInfoExport_' + DateTime.now.strftime('%Y-%m-%d') + '.xlsx'
-      additional_files = []
-      create_file(job_id, csv_name, temp_report.path, excel_name, temp_excel_report.path, password, user_id, course_id, additional_files)
+      create_file(job_id, csv_name, temp_report.path, false, false, password, user_id, course_id, nil)
     rescue => error
       Sidekiq.logger.error error.inspect
       job.status = 'failing' +  error.inspect
       job.save
       File.delete(temp_report) if File.exist?(temp_report)
-      File.delete(temp_excel_report) if File.exist?(temp_excel_report)
     end
   end
 
-
   private
 
-  def create_report(job_id, privacy_flag, combined_enrollment_info_flag=false, use_course_state = false)
-    pager = 1
+  def create_report(job_id, privacy_flag, combined_enrollment_info_flag = false, use_course_state = false)
     file = Tempfile.open(job_id.to_s, get_tempdir)
     csv = CSV.new(file)
     @filepath = File.absolute_path(file)
-    excel_tmp_file =  Tempfile.new('excel_user_export')
-    headers = []
-    user_info = []
-    courses = []
-    Xikolo::Course::Course.each_item(affiliated: 'true', public: 'true') do |item|
-      courses << item
+
+    courses = {}
+    Xikolo::Course::Course.each_item(affiliated: 'true', public: 'true') do |course|
+      courses[course.id] = course
     end
     Acfs.run
-    all_courses = {}
 
-    courses.each do |c|
-      all_courses[c.id] = c
-    end
-
-    #$stdout.print all_courses.inspect
     Sidekiq.logger.debug "Writing export to #{@filepath}"
-    #csv << ["row", "of", "CSV", "data"]
 
+    pager = 1
     loop do
       begin
         users = Xikolo::Account::User.where(confirmed: true, page: pager, per_page: 500)
         Acfs.run
+
         if users.current_page == 1
+          # write header
+
           presenter = Account::ProfilePresenter.new users.first
           Acfs.run
-          #write header
-          unless privacy_flag
-            headers += ['User ID',
-                 'First Name',
-                 'Last name',
-                 'Email',
-                 'Language',
-                 'Affiliated',
-                 'BirthDate',
-                 'TopCountry',
-                 'FirstEnrollment',
-                 *presenter.fields.map{|f|f.name},
-                 *courses.map{|c|c.course_code}
-            ]
-          else
-            headers += ['User ID',
-                  'Language',
-                  'Affiliated',
-                  'Created',
-                  'BirthDate',
-                  'TopCountry',
-                  'FirstEnrollment',
-                  *presenter.fields.map{|f|f.name},
-                  *courses.map{|c|c.course_code}
-            ]
-          end
-          csv << headers
 
-        end
-        p = 0
-        tmp_holder = {}
-        user_courses = {}
-        enrollments = {}
-        profiles = {}
-        users.each do |user|
-          p += 1
-          update_progress(users, job_id, p)
-          enrollments[user.id] = Xikolo::Course::Enrollment.where(user_id: user.id, learning_evaluation: 'true', deleted: 'true', per_page: 200)
-          profiles[user.id] = Account::ProfilePresenter.new user
-          user_courses[user.id] = {}
-          user_courses[user.id].each do |course|
-            user_courses[user.id][course.id] = ''
+          headers = ['User ID']
+
+          unless privacy_flag
+            headers += ['First Name',
+                        'Last name',
+                        'Email']
           end
-          #$stdout.print user_courses.inspect
-          Acfs.run
+
+          headers += ['Language',
+                      'Affiliated',
+                      'Created',
+                      'BirthDate',
+                      'TopCountry',
+                      'FirstEnrollment',
+                      *presenter.fields.map { |f| f.name },
+                      *courses.values.map { |c| c.course_code }]
+
+          csv << headers
+          csv.flush
         end
-        Acfs.run
-        #iterate again
-        users.each do |user|
-          enrollments[user.id].each do |enrollment|
+
+        users.each.with_index(1) do |user, index|
+          update_progress(users, job_id, index)
+
+          user_enrollments = Xikolo::Course::Enrollment.where(user_id: user.id, learning_evaluation: 'true', deleted: 'true', per_page: 200)
+          user_profile = Account::ProfilePresenter.new user
+          Acfs.run
+
+          user_course_states = {}
+
+          user_enrollments.each do |enrollment|
             if combined_enrollment_info_flag
               # '': not enrolled
               # e: enrolled
@@ -114,85 +86,63 @@ class CreateUserInfoExportJob < CreateExportJob
               state = 'p' if enrollment.points.present? and enrollment.points[:percentage].to_f > 0
               state = 'c' if enrollment.completed
               state = 'r' if enrollment.certificates.present? and enrollment.certificates[:record_of_achievement]
-              user_courses[user.id][enrollment.course_id] = state
+              user_course_states[enrollment.course_id] = state
             else
-              user_courses[user.id][enrollment.course_id] = enrollment.points ? enrollment.points[:percentage] : '-'
+              user_course_states[enrollment.course_id] = enrollment.points ? enrollment.points[:percentage] : '-'
             end
           end
-          if enrollments[user.id].count > 0
-           course_id = enrollments[user.id].sort_by { |c| c.created_at }.first.course_id
-           if course_id.present? && all_courses[course_id].present? && all_courses[course_id].course_code.present?
-              first_enrollment = all_courses[course_id].course_code
-            else
-              first_enrollment = ''
-            end
 
-          else
-            first_enrollment = ''
+          first_enrollment = ''
+          if user_enrollments.count > 0
+            course_id = user_enrollments.sort_by { |c| c.created_at }.first.course_id
+            if course_id.present? && courses[course_id].present? && courses[course_id].course_code.present?
+              first_enrollment = courses[course_id].course_code
+            end
           end
 
           begin
-            top_country =  Lanalytics::Metric::UserCourseCountry.query(
-                user.id,
-                nil,
-                nil,
-                nil,
-                nil,
-                nil,
-                nil)
+            top_country = Lanalytics::Metric::UserCourseCountry.query(user.id, nil, nil, nil, nil, nil, nil)
           rescue => error
             Sidekiq.logger.error error.inspect
             top_country = ''
           end
-          values = []
-          unless privacy_flag
-            values += [user.id,
-                   user.first_name,
-                   user.last_name,
-                   user.email,
-                   user.language,
-                   user.affiliated,
-                   user.born_at,
-                   top_country,
-                   first_enrollment,
-                   *profiles[user.id].fields.map{|f|f.value},
-                   *courses.map{|c|user_courses[user.id][c.id].present? ? user_courses[user.id][c.id] : 'n'}
-            ]
-          else
-            values += [user.id,
-                   user.language,
-                   user.affiliated,
-                   user.created_at.strftime('%Y-%m-%d'),
-                   user.born_at,
-                   top_country,
-                   first_enrollment,
-                   *profiles[user.id].fields.map{|f|f.value},
-                   *courses.map{|c|user_courses[user.id][c.id].present? ? user_courses[user.id][c.id] : 'n'}
-            ]
-          end
-          csv << values
-          user_info << values
 
+          values = [user.id]
+
+          unless privacy_flag
+            values += [user.first_name,
+                       user.last_name,
+                       user.email]
+          end
+
+          values += [user.language,
+                     user.affiliated,
+                     user.created_at.strftime('%Y-%m-%d'),
+                     user.born_at,
+                     top_country,
+                     first_enrollment,
+                     *user_profile.fields.map { |f| f.value },
+                     *courses.values.map { |c| user_course_states[c.id].present? ? user_course_states[c.id] : '-'}]
+
+          csv << values
+          csv.flush
         end
 
-        Sidekiq.logger.debug "fetching page #{pager.to_s}"
-        pager = pager+1
+        Sidekiq.logger.debug "Fetching page #{pager.to_s}"
+        pager = pager + 1
         break if (users.current_page >= users.total_pages)
 
       rescue Exception => error
         Sidekiq.logger.error error.message
       end
-    end
-      excel_file = excel_attachment('UserInfoExport', excel_tmp_file, headers, user_info)
 
-      Acfs.run
-      return file, excel_file
+    end
+
+    Acfs.run
+    return file
 
   ensure
     file.close
-    excel_file.close
   end
+
 end
-
-
-
