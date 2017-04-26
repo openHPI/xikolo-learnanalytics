6871288a1a71e54618316e793376b1092ca53577
jan <jan.renz@hpi.de>
2017-04-26 13:01:04 +0200

    check if there is a result from es

--- app/jobs/quiz_performance_worker.rb
+++ app/jobs/quiz_performance_worker.rb
@@ -8,16 +8,18 @@ class QuizPerformanceWorker < QcRuleWorker
     Acfs.run
     items.each do |item|
       quiz_performance = calculate_metrics(item, course)
-      avg_points = quiz_performance["average_points_percentage"].round(2)
-      avg_attempts = quiz_performance["avg_attempts"].round(2)
-      first_attempt_avg_points = quiz_performance["average_points_percentage_first_attempt"].round(2)
-      total = quiz_performance["total"]
-      if total >= 10
-        if avg_attempts >=  avg_attempts_threshold or avg_points <= avg_percentage_threshold or first_attempt_avg_points <= avg_first_attempt_percentage_threshold
-          annotation = "Quiz performance low: avg attempts #{avg_attempts}, avg_performance: #{avg_points}, first_attempt_avg: #{first_attempt_avg_points}"
-          update_or_create_qc_alert_with_data(rule_id, course.id, severity, annotation + avg_points.to_s, item.id, create_json(item.id))
-        else
-          find_and_close_qc_alert_with_data(rule_id, course.id, item.id)
+      if quiz_performance
+        avg_points = quiz_performance["average_points_percentage"].round(2)
+        avg_attempts = quiz_performance["avg_attempts"].round(2)
+        first_attempt_avg_points = quiz_performance["average_points_percentage_first_attempt"].round(2)
+        total = quiz_performance["total"]
+        if total >= 10
+          if avg_attempts >=  avg_attempts_threshold or avg_points <= avg_percentage_threshold or first_attempt_avg_points <= avg_first_attempt_percentage_threshold
+            annotation = "Quiz performance low: avg attempts #{avg_attempts}, avg_performance: #{avg_points}, first_attempt_avg: #{first_attempt_avg_points}"
+            update_or_create_qc_alert_with_data(rule_id, course.id, severity, annotation + avg_points.to_s, item.id, create_json(item.id))
+          else
+            find_and_close_qc_alert_with_data(rule_id, course.id, item.id)
+          end
         end
       end
     end
