cf987c175ef40274b149cf407e8382254cd5b056
Franz Liedke <franz.liedke@student.hpi.de>
2017-05-08 14:30:30 +0200

    Prepare service for use with Docker setup

--- config/initializers/acfs.rb
+++ config/initializers/acfs.rb
@@ -1,5 +1,1 @@
-# Load service configuration
-Acfs.configure do
-  load 'config/services.yml'
-  load "config/services.#{Rails.env}.yml" if File.exist? "config/services.#{Rails.env}.yml"
-end
+# Empty due to deployment bug
