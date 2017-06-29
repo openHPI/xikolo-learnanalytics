3ed57f2d24e41ee4a386e54da552de62ca844bcf
Franz Liedke <franz.liedke@student.hpi.de>
2017-06-29 10:53:19 +0200

    CourseEvents Metric: Fix usage of scroll API

--- lib/lanalytics/metric/course_events.rb
+++ lib/lanalytics/metric/course_events.rb
@@ -38,7 +38,7 @@ module Lanalytics
           end
         else
           result = datasource.exec do |client|
-            client.scroll scroll: '5m', scroll_id: scroll_id
+            client.scroll scroll: '5m', scroll_id: scroll_id, body: { scroll_id: scroll_id }
           end
         end
 
