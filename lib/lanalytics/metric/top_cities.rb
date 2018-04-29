638dd23c1ddbef36078be2c8e30784b0a8743503
Sören Oldag <soeren_oldag@freenet.de>
2018-04-29 21:44:54 +0200

    Add TopCities metric.

---
+++ lib/lanalytics/metric/top_cities.rb
@@ -0,0 +1,100 @@
+module Lanalytics
+  module Metric
+    class TopCities < ExpApiMetric
+
+      description 'Returns top 100 cities.'
+
+      optional_parameter :course_id
+
+      exec do |params|
+        course_id = params[:course_id]
+
+        # array of mobile runtimes
+        mobile_runtimes = %w(Android iOS)
+
+        mobile_conditions = mobile_runtimes.map do |runtime|
+          { match: { 'in_context.runtime' => runtime } }
+        end
+
+        result = datasource.exec do |client|
+          body = {
+              size: 0,
+              aggregations: {
+                  countries: {
+                      terms: {
+                          field: 'in_context.user_location_country_code',
+                          size: 100
+                      },
+                      aggregations: {
+                          cities: {
+                              terms: {
+                                  field: 'in_context.user_location_city',
+                                  size: 100
+                              },
+                              aggregations: {
+                                  ucount: {
+                                      cardinality: {
+                                          field: 'user.resource_uuid'
+                                      }
+                                  },
+                                  mobile: {
+                                      filter: {
+                                          bool: {
+                                              must: { exists: { field: 'in_context.runtime' } },
+                                              should: mobile_conditions,
+                                              minimum_should_match: 1
+                                          }
+                                      },
+                                      aggs: {
+                                          count: {
+                                              cardinality: {
+                                                  field: 'user.resource_uuid'
+                                              }
+                                          }
+                                      }
+                                  }
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+
+          if course_id.present?
+            body[:query] = {
+                bool: {
+                    must: [
+                        { match: { 'course_id' => course_id } }
+                    ]
+                }
+            }
+          end
+
+          client.search index: datasource.index, body: body
+        end
+
+        processed_result = []
+        # process result
+        result['aggregations']['countries']['buckets'].each do |country_item|
+          country_item['cities']['buckets'].each do |city_item|
+            begin
+              result_subitem = {}
+              result_subitem[:city_name] = city_item['key']
+              result_subitem[:country_code] = country_item['key']
+              result_subitem[:country_code_iso3] = IsoCountryCodes.find(country_item['key']).alpha3
+              result_subitem[:total_activity] = city_item['doc_count']
+              result_subitem[:distinct_users] = city_item['ucount']['value']
+              result_subitem[:activity_per_user] = city_item['ucount']['value'] != 0 ? city_item['doc_count'] / city_item['ucount']['value'] : 0
+              result_subitem[:mobile_users] = city_item['mobile']['count']['value']
+              result_subitem[:mobile_usage] = city_item['ucount']['value'] != 0 ? city_item['mobile']['count']['value'].to_f / city_item['ucount']['value'].to_f : 0
+              processed_result << result_subitem
+            rescue IsoCountryCodes::UnknownCodeError
+            end
+          end
+        end
+        processed_result.sort_by { |i| i[:distinct_users] }.reverse
+      end
+
+    end
+  end
+end
