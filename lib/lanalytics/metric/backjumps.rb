949fe0d75d974f6456c85399f77727b110ab92dd
Sören Oldag <soeren_oldag@freenet.de>
2018-07-02 14:08:51 +0200

    Access restify responses with string keys.

--- lib/lanalytics/metric/backjumps.rb
+++ lib/lanalytics/metric/backjumps.rb
@@ -10,10 +10,10 @@ module Lanalytics
       optional_parameter :start_date, :end_date, :item_id
 
       exec do |params|
-        sections_index = get_sections(params[:course_id]).map { |section| [section.id, section] }.to_h
+        sections_index = get_sections(params[:course_id]).map { |section| [section['id'], section] }.to_h
         all_items = get_items(params[:course_id])
-        all_items = all_items.sort_by { |item| [sections_index[item.section_id].position, item.position] }
-        items = params[:item_id].present? ? all_items.select{ |item| item.id == params[:item_id] } : all_items
+        all_items = all_items.sort_by { |item| [sections_index[item['section_id']]['position'], item['position']] }
+        queried_items = params[:item_id].present? ? all_items.select{ |item| item['id'] == params[:item_id] } : all_items
         next {items: []} if all_items.empty?
 
         # Total visits per course item
@@ -22,23 +22,23 @@ module Lanalytics
             {
               dimension_name: 'ga:dimension2',
               operator: 'IN_LIST',
-              expressions: items.map(&:id)
+              expressions: queried_items.map { |item| item['id'] }
             }
           ]
         })
         total_visits = result[:rows].map {|row| [row['ga:dimension2'], row['ga:pageviews']]}.to_h
 
         # Backjump visits per course item
-        result = request_reports(items.map do |item|
+        result = request_reports(queried_items.map do |item|
           succeeding_items = all_items.drop(all_items.index(item) + 1)
           next if succeeding_items.empty?
 
-          pageviews_report_request(params, item_filter(item.id), {
+          pageviews_report_request(params, item_filter(item['id']), {
             filters: [
               {
                 dimension_name: 'ga:previousPagePath',
                 operator: 'IN_LIST',
-                expressions: succeeding_items.map {|prev_item| "/courses/#{params[:course_id]}/item/#{prev_item.id}"}
+                expressions: succeeding_items.map {|prev_item| "/courses/#{params[:course_id]}/item/#{prev_item['id']}"}
               }
             ]
           })
@@ -49,11 +49,11 @@ module Lanalytics
 
         # Process results
         processed_result = {}
-        processed_result[:items] = items.map do |item|
-          item_backjumps = total_backjumps[item.id] || 0
-          item_visits = total_visits[item.id] || 0
+        processed_result[:items] = queried_items.map do |item|
+          item_backjumps = total_backjumps[item['id']] || 0
+          item_visits = total_visits[item['id']] || 0
           {
-            item_id: item.id,
+            item_id: item['id'],
             total_visits: item_visits,
             total_backjumps: item_backjumps,
             relative_backjumps: item_visits == 0 ? 0 : item_backjumps.percent_of(item_visits)
