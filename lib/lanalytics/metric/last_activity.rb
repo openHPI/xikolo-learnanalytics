eb8d397842bc4a36c1197dd078d3e80933f56501
Tobias Rohloff <tobias.rohloff@hpi.de>
2018-08-30 16:18:23 +0200

    Add last activity metric for user
    Fix XI-3571

---
+++ lib/lanalytics/metric/last_activity.rb
@@ -0,0 +1,32 @@
+module Lanalytics
+  module Metric
+    class LastActivity < ExpApiMetric
+
+      description 'Last activity of user.'
+
+      required_parameter :user_id
+
+      optional_parameter :course_id
+
+      exec do |params|
+        result = datasource.exec do |client|
+          client.search index: datasource.index, body: {
+            size: 1,
+            query: {
+              bool: {
+                must: all_filters(params[:user_id], params[:course_id], nil),
+              }
+            },
+            sort: {
+              timestamp: {
+                order: 'desc'
+              }
+            }
+          }
+        end
+        result.dig('hits', 'hits', 0, '_source') || {}
+      end
+
+    end
+  end
+end
