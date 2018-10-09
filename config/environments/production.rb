08c7314d36288a9cfa94acf7ad34621c8c06cac6
Tobias Rohloff <tobias.rohloff@hpi.de>
2018-10-09 10:59:26 +0200

    Cleanup application frameworks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
+  config.action_mailer.perform_caching = false
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
