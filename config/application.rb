661ca4b7f60b9ceffde2b2058d2ea58b6d7f69c9
Tobias Rohloff <tobias.rohloff@hpi.de>
2018-10-09 09:55:34 +0200

    Adjust application config

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,10 @@ Bundler.require(*Rails.groups)
 module Xikolo::Lanalytics
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths += %W(#{config.root}/lib)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
