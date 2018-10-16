739beaa0811e18ab578b6c2eceed5caae6b2df12
Tobias Rohloff <tobias.rohloff@hpi.de>
2018-10-16 13:46:16 +0200

    Disable protect from forgery globally

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module Xikolo::Lanalytics
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.action_controller.default_protect_from_forgery = false
   end
 
   def self.rake?
