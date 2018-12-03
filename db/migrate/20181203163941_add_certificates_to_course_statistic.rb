953a1500c4b1bdcaf3471b31409ed6a999bb0923
Tobias Rohloff <tobias.rohloff@hpi.de>
2018-12-03 18:30:21 +0100

    Add certificate statistics to course statistics

---
+++ db/migrate/20181203163941_add_certificates_to_course_statistic.rb
@@ -0,0 +1,8 @@
+class AddCertificatesToCourseStatistic < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :course_statistics, :certificates, :roa_count
+    add_column :course_statistics, :cop_count, :integer
+    add_column :course_statistics, :qc_count, :integer
+    add_column :course_statistics, :consumption_rate, :float
+  end
+end
