91f0e2c8c3337a493b845f14be343b32f6550fd3
Tobias Rohloff <tobias.rohloff@hpi.de>
2019-01-24 16:01:34 +0100

    Fix user session statistics metric for learner dashboard

--- lib/lanalytics/metric/user_session_statistics.rb
+++ lib/lanalytics/metric/user_session_statistics.rb
@@ -1,14 +1,51 @@
 module Lanalytics
   module Metric
-    class UserSessionStatistics < ExpApiMetric
+    class UserSessionStatistics < Base
 
       description 'Returns all session statistics for the user as well as all course participants by querying the respective session metrics.'
 
       required_parameter :course_id, :user_id
 
+      def self.datasource_keys
+        %w(exp_api_elastic exp_api_native google_analytics)
+      end
+
       exec do |params|
         course_id = params[:course_id]
         user_id = params[:user_id]
+
+        # we need to query the number of users with events in course, to
+        # calculate the average per user for Google Analytics metrics
+        elastic = Lanalytics::Processing::DatasourceManager.datasource('exp_api_elastic')
+        user_result = elastic.exec do |client|
+          client.search index: elastic.index, body: {
+            size: 0,
+            query: {
+              bool: {
+                must: [
+                  {
+                    bool: {
+                      minimum_should_match: 1,
+                      should: [
+                        { match: { 'in_context.course_id' => course_id } },
+                        { match: { 'resource.resource_uuid' => course_id } }
+                      ]
+                    }
+                  }
+                ]
+              }
+            },
+            aggs: {
+              distinct_user_count: {
+                cardinality: {
+                  field: 'user.resource_uuid',
+                  precision_threshold: 40_000
+                }
+              }
+            }
+          }
+        end
+        user_count = user_result['aggregations']['distinct_user_count']['value'].to_f
 
         session_durations = SessionDurations.query(course_id: course_id)
 
@@ -17,8 +54,8 @@ module Lanalytics
           user_total_sessions: TotalSessionDuration.query(course_id: course_id, user_id: user_id),
           user_sessions: Sessions.query(course_id: course_id, user_id: user_id),
           course_avg_sessions: session_durations[:avg_session_duration],
-          course_total_sessions: session_durations[:total_session_duration],
-          course_sessions: session_durations[:total_sessions]
+          course_total_sessions: session_durations[:total_session_duration] / user_count,
+          course_sessions: session_durations[:total_sessions] / user_count
         }
       end
     end
