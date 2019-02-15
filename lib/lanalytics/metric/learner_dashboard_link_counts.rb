74f83a381b3f1a95045f857ffbf3eafaa75ee434
Dominic Sauer <dominic.sauer@student.hpi.uni-potsdam.de>
2019-02-15 16:07:04 +0100

    Add dates filter for learner dashboard link counts

--- lib/lanalytics/metric/learner_dashboard_link_counts.rb
+++ lib/lanalytics/metric/learner_dashboard_link_counts.rb
@@ -4,7 +4,7 @@ module Lanalytics
 
       description 'Counts the number of clicks on links (to forum, items, sections) in the learner dashboard.'
 
-      optional_parameter :user_id, :course_id
+      optional_parameter :user_id, :course_id, :start_date, :end_date
 
       exec do |params|
         {
@@ -17,8 +17,11 @@ module Lanalytics
       end
 
       def self.count_for_verb(params, verb)
+        start_date = params[:start_date]
+        end_date = params[:end_date]
+
         datasource.exec do |client|
-          body = {
+          query = {
             query: {
               bool: {
                 must: [
@@ -27,8 +30,16 @@ module Lanalytics
               }
             }
           }
+          query[:query][:bool][:filter] = {
+            range: {
+              timestamp: {
+                gte: DateTime.parse(start_date).iso8601,
+                lte: DateTime.parse(end_date).iso8601
+              }
+            }
+          } if start_date.present? and end_date.present?
 
-          client.count index: datasource.index, body: body
+          client.count index: datasource.index, body: query
         end.fetch('count')
       end
     end
