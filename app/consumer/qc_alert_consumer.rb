52a081b77bb7a6922e22aba04f8a7ddf507e4fd6
Franz Liedke <franz.liedke@student.hpi.de>
2019-03-07 16:57:30 +0100

    Comment out broken consumer

--- app/consumer/qc_alert_consumer.rb
+++ app/consumer/qc_alert_consumer.rb
@@ -1,12 +1,12 @@
 class QcAlertConsumer < Msgr::Consumer
 
   def update_course
-    course_id = payload.fetch :id
-    course_deleted = payload.fetch :deleted
+    #course_id = payload.fetch :id
+    #course_deleted = payload.fetch :deleted
 
-    if course_deleted
-      QcAlert.where(course_id: course_id).destroy_all
-    end
+    #if course_deleted
+    #  QcAlert.where(course_id: course_id).destroy_all
+    #end
   end
 
 end
