f8ea3d43baa7a9017a34d89de75f2cb3b146fcb3
Tobias Rohloff <tobias.rohloff@hpi.de>
2019-04-18 11:25:54 +0200

    Adjust top cities metric

--- lib/lanalytics/metric/top_cities_es.rb
+++ lib/lanalytics/metric/top_cities_es.rb
@@ -10,13 +10,6 @@ module Lanalytics
       exec do |params|
         course_id = params[:course_id]
 
-        # array of mobile runtimes
-        mobile_runtimes = %w(Android iOS)
-
-        mobile_conditions = mobile_runtimes.map do |runtime|
-          { match: { 'in_context.runtime' => runtime } }
-        end
-
         result = datasource.exec do |client|
           client.search index: datasource.index, body: {
               size: 0,
@@ -65,22 +58,6 @@ module Lanalytics
                                                       cardinality: {
                                                           field: 'user.resource_uuid'
                                                       }
-                                                  },
-                                                  mobile: {
-                                                      filter: {
-                                                          bool: {
-                                                              must: { exists: { field: 'in_context.runtime' } },
-                                                              should: mobile_conditions,
-                                                              minimum_should_match: 1
-                                                          }
-                                                      },
-                                                      aggs: {
-                                                          count: {
-                                                              cardinality: {
-                                                                  field: 'user.resource_uuid'
-                                                              }
-                                                          }
-                                                      }
                                                   }
                                               }
                                           }
@@ -105,12 +82,8 @@ module Lanalytics
             result_subitem[:city_name] = city_item['key'].capitalize
             result_subitem[:country_code] = country_item['key']
             result_subitem[:country_code_iso3] = IsoCountryCodes.find(country_item['key']).alpha3
-            result_subitem[:total_activity] = country_item['doc_count']
             result_subitem[:distinct_users] = country_item['ucount']['value']
             result_subitem[:relative_users] = result_subitem[:distinct_users].percent_of(total_users)
-            result_subitem[:activity_per_user] = country_item['ucount']['value'] != 0 ? country_item['doc_count'] / country_item['ucount']['value'] : 0
-            result_subitem[:mobile_users] = country_item['mobile']['count']['value']
-            result_subitem[:mobile_usage] = country_item['ucount']['value'] != 0 ? country_item['mobile']['count']['value'].to_f / country_item['ucount']['value'].to_f : 0
             processed_result << result_subitem
           rescue IsoCountryCodes::UnknownCodeError
           end
