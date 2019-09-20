cf67976f5233196272358cef5df7bdecfa2e0a4e
Tobias Rohloff <tobias.rohloff@hpi.de>
2019-09-20 15:29:11 +0200

    Add rich text links metric

---
+++ lib/lanalytics/metric/rich_text_links.rb
@@ -0,0 +1,73 @@
+module Lanalytics
+  module Metric
+    class RichTextLinks < ExpApiMetric
+
+      description 'Stats for all links of a rich text item.'
+
+      required_parameter :item_id
+
+      exec do |params|
+        body = {
+          size: 0,
+          query: {
+            bool: {
+              must: [
+                { match: { tracking_type: 'rich_text_item_link' } },
+                { match: { tracking_id: params[:item_id] } }
+              ]
+            }
+          },
+          aggs: {
+            links: {
+              terms: {
+                field: 'tracking_external_link',
+                size: 10_000
+              },
+              aggs: {
+                user: {
+                  cardinality: {
+                    field: 'user_id',
+                    precision_threshold: 40_000
+                  }
+                },
+                earliest_timestamp: {
+                  top_hits: {
+                    sort: [
+                      { timestamp: { order: 'asc' } }
+                    ],
+                    _source: ['timestamp'],
+                    size: 1
+                  }
+                },
+                latest_timestamp: {
+                  top_hits: {
+                    sort: [
+                      { timestamp: { order: 'desc' } }
+                    ],
+                    _source: ['timestamp'],
+                    size: 1
+                  }
+                }
+              }
+            }
+          }
+        }
+
+        result = datasource.exec do |client|
+          client.search index: datasource.index, body: body
+        end
+
+        result.dig('aggregations', 'links', 'buckets').map do |item|
+          {
+            link: item['key'],
+            total_clicks: item.dig('doc_count').to_i,
+            total_clicks_unique_users: item.dig('user', 'value').to_i,
+            earliest_timestamp: item.dig('earliest_timestamp', 'hits', 'hits', 0, '_source', 'timestamp'),
+            latest_timestamp: item.dig('latest_timestamp', 'hits', 'hits', 0, '_source', 'timestamp')
+          }
+        end
+      end
+
+    end
+  end
+end
