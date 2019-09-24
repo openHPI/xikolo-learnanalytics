634c7e92910841d14baa1ba080ba6889cc773e86
Tobias Rohloff <tobias.rohloff@hpi.de>
2019-09-24 14:44:39 +0200

    Add profile fields disclosure link click metrics

---
+++ lib/lanalytics/metric/profile_fields_disclosure_link_click.rb
@@ -0,0 +1,32 @@
+module Lanalytics
+  module Metric
+    class ProfileFieldsDisclosureLinkClick < ExpApiMetric
+
+      description 'Check if a user has clicked on the profile_fields_disclosure_link (A/B test)'
+
+      required_parameter :course_id, :user_id
+
+      exec do |params|
+        body = {
+          size: 0,
+          query: {
+            bool: {
+              must: [
+                { match: { tracking_type: 'profile_fields_disclosure_link' } },
+                { match: { course_id: params[:course_id] } },
+                { match: { user_id: params[:user_id] } }
+              ]
+            }
+          }
+        }
+
+        result = datasource.exec do |client|
+          client.search index: datasource.index, body: body
+        end
+
+        { clicked: result.dig('hits', 'total').to_i.positive? }
+      end
+
+    end
+  end
+end
