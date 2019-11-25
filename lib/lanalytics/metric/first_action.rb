ee208208c43f9a0aa1f8ea41b952e03f096b85bc
Tobias Rohloff <tobias.rohloff@hpi.de>
2019-11-25 14:01:56 +0100

    Set lower bound of first action metric result set to course start date

--- lib/lanalytics/metric/first_action.rb
+++ lib/lanalytics/metric/first_action.rb
@@ -1,30 +1,36 @@
+# frozen_string_literal: true
+
 module Lanalytics
   module Metric
     class FirstAction < ExpApiMetric
-
       description 'First action of a user in a course.'
 
       required_parameter :course_id, :user_id
 
       exec do |params|
+        course_service = Xikolo.api(:course).value!
+
+        course = course_service.rel(:course).get(id: params[:course_id]).value!
+
         result = datasource.exec do |client|
           client.search index: datasource.index, body: {
             size: 1,
             query: {
               bool: {
-                must: [] + all_filters(params[:user_id], params[:course_id], nil),
-              }
+                must: [
+                  {range: {timestamp: {gte: course['start_date']}}},
+                ] + all_filters(params[:user_id], params[:course_id], nil),
+              },
             },
             sort: {
               timestamp: {
-                order: 'asc'
-              }
-            }
+                order: 'asc',
+              },
+            },
           }
         end
         result.dig('hits', 'hits', 0, '_source') || {}
       end
-
     end
   end
 end
