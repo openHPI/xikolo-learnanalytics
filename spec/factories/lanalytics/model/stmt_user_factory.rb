55bd7295523975bdcb95c8f44dc592072bf52916
Franz Liedke <franz.liedke@hpi.de>
2019-12-03 17:28:18 +0100

    Delete obsolete factories

--- spec/factories/lanalytics/model/stmt_user_factory.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :stmt_user, class: Lanalytics::Model::StmtUser do
-    uuid { '00000003-3100-4444-9999-1234567890' }
-    initialize_with { new(uuid) }
-  end
-end
--- spec/factories/lanalytics/model/stmt_verb_factory.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :stmt_verb, class: Lanalytics::Model::StmtVerb do
-    type { 'SOME_VERB' }
-    initialize_with { new(type) }
-  end
-end
