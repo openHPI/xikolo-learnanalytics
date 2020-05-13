c527a43e2b8b0189e08e067577d1b8087d3cd5ef
Tobias Rohloff <tobias.rohloff@hpi.de>
2020-05-13 18:51:15 +0200

    Add new filters to elasticsearch base metric class

--- lib/lanalytics/metric/exp_events_elastic_metric.rb
+++ lib/lanalytics/metric/exp_events_elastic_metric.rb
@@ -3,7 +3,6 @@
 module Lanalytics
   module Metric
     class ExpEventsElasticMetric < Base
-
       def self.datasource_keys
         %w[exp_events_elastic]
       end
@@ -12,26 +11,75 @@ module Lanalytics
         datasources.first
       end
 
-      def self.all_filters(user_id, course_id, resource_id)
-        filters_ = course_id.nil? ? [] : [
-          { bool: {
-              minimum_should_match: 1,
-              should: [
-                { match: { 'in_context.course_id' => course_id } },
-                { match: { 'resource.resource_uuid' => course_id } }
-              ]
-            }
-          }
-        ]
-        filters_ += user_id.nil? ? [] : [ { match: { 'user.resource_uuid' => user_id } } ]
-        filters_ += resource_id.nil? ? [] : [ { match: { 'resource.resource_uuid' => resource_id } } ]
-        filters_ + filters
+      def self.course_filter(course_id)
+        return nil if course_id.nil?
+
+        {
+          bool: {
+            minimum_should_match: 1,
+            should: [
+              {match: {'in_context.course_id' => course_id}},
+              {match: {'resource.resource_uuid' => course_id}},
+            ],
+          },
+        }
       end
 
+      def self.date_filter(start_date, end_date)
+        return nil if start_date.nil? && end_date.nil?
+
+        df = {range: {timestamp: {}}}
+
+        if start_date.present?
+          df[:range][:timestamp][:gte] = DateTime.parse(start_date).iso8601
+        end
+
+        if end_date.present?
+          df[:range][:timestamp][:lte] = DateTime.parse(end_date).iso8601
+        end
+
+        df
+      end
+
+      # deprecated
+      # rubocop:disable Metrics/MethodLength
+      def self.all_filters(user_id, course_id, resource_id)
+        filters_ = if course_id.nil?
+                     []
+                   else
+                     [
+                       {
+                         bool: {
+                           minimum_should_match: 1,
+                           should: [
+                             {match: {'in_context.course_id' => course_id}},
+                             {match: {'resource.resource_uuid' => course_id}},
+                           ],
+                         },
+                       },
+                     ]
+                   end
+
+        filters_ += if user_id.nil?
+                      []
+                    else
+                      [{match: {'user.resource_uuid' => user_id}}]
+                    end
+
+        filters_ += if resource_id.nil?
+                      []
+                    else
+                      [{match: {'resource.resource_uuid' => resource_id}}]
+                    end
+
+        filters_ + filters
+      end
+      # rubocop:enable all
+
+      # deprecated
       def self.filters
         []
       end
-
     end
   end
 end
