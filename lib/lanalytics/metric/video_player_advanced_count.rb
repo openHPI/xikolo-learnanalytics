873bb921abd1bd4598ccfe8e618a8f134680d0f3
Dominic Sauer <dominic.sauer@hpi.de>
2020-07-27 11:59:59 +0200

    Remove VideoPlayerAdvancedCount metric

--- lib/lanalytics/metric/video_player_advanced_count.rb
+++
@@ -1,9 +0,0 @@
-module Lanalytics
-  module Metric
-    class VideoPlayerAdvancedCount < ExpEventsCountElasticMetric
-
-      event_verbs %w(VIDEO_CHANGE_SIZE VIDEO_CHANGE_SPEED VIDEO_FULLSCREEN)
-
-    end
-  end
-end
