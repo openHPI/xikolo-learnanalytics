d7687353ae2d5b1e53a34b0cbfa17a85c3a3f905
Tobias Rohloff <tobias.rohloff@hpi.de>
2020-10-22 16:48:10 +0200

    Refactor device usage metric

--- lib/lanalytics/metric/device_usage.rb
+++ lib/lanalytics/metric/device_usage.rb
@@ -1,62 +1,51 @@
+# frozen_string_literal: true
+
 module Lanalytics
   module Metric
     class DeviceUsage < ExpEventsElasticMetric
       include Lanalytics::Helper::PercentageHelper
       extend Lanalytics::Helper::ClientUsageHelper
 
-      description 'Returns the used platforms and runtimes, as well as an aggregated behavior state, which is either web, mobile or mixed.'
+      description <<~DOC
+        Returns the used platforms and runtimes, as well as an aggregated behavior state, which is either web, mobile or mixed.
+      DOC
 
       optional_parameter :user_id, :course_id
 
       exec do |params|
-        query_must = []
-        if params[:user_id].present?
-          query_must << [
-            { match: { 'user.resource_uuid' => params[:user_id] } }
-          ]
-        end
-
-        if params[:course_id].present?
-          query_must << {
-            bool: {
-              should: [
-                { match: { 'in_context.course_id' => params[:course_id] } },
-                { match: { 'resource.resource_uuid' => params[:course_id] } }
-              ]
-            }
-          }
-        end
-
         result = datasource.exec do |client|
           client.search index: datasource.index, body: {
             size: 0,
             query: {
               bool: {
-                must: query_must
-              }
+                must: [
+                  course_filter(params[:user_id]),
+                  user_filter(params[:course_id]),
+                ].compact,
+              },
             },
             aggregations: {
               platforms: {
                 terms: {
                   field: 'in_context.platform',
-                  size: 50
+                  size: 50,
                 },
                 aggregations: {
                   runtimes: {
                     terms: {
                       field: 'in_context.runtime',
-                      size: 50
-                    }
-                  }
-                }
+                      size: 50,
+                    },
+                  },
+                },
               },
               runtimes: {
                 terms: {
                   field: 'in_context.runtime',
-                  size: 50
-                }
-              }
-            }
+                  size: 50,
+                },
+              },
+            },
           }
         end
 
@@ -68,7 +57,9 @@ module Lanalytics
 
         # total activity
         total_activity = 0
-        result['aggregations']['platforms']['buckets'].each { |item| total_activity += item['doc_count'] }
+        result['aggregations']['platforms']['buckets'].each do |item|
+          total_activity += item['doc_count']
+        end
 
         # process platforms
         platforms = []
@@ -83,7 +74,8 @@ module Lanalytics
           result_subitem = {}
           result_subitem[:platform] = platform['key']
           result_subitem[:total_activity] = platform['doc_count']
-          result_subitem[:relative_activity] = platform['doc_count'].percent_of total_activity
+          result_subitem[:relative_activity] =
+            platform['doc_count'].percent_of(total_activity)
 
           # process nested runtimes for platform
           runtimes = []
@@ -91,19 +83,20 @@ module Lanalytics
             result_subsubitem = {}
             result_subsubitem[:runtime] = runtime['key']
             result_subsubitem[:total_activity] = runtime['doc_count']
-            result_subsubitem[:relative_activity] = runtime['doc_count'].percent_of platform['doc_count']
+            result_subsubitem[:relative_activity] =
+              runtime['doc_count'].percent_of(platform['doc_count'])
 
             runtimes << result_subsubitem
           end
 
           # sort and add nested runtimes for platform
-          runtimes.sort_by! { |i| i[:total_activity] }.reverse!
+          runtimes.sort_by! {|i| i[:total_activity] }.reverse!
           result_subitem[:runtimes] = runtimes
 
           platforms << result_subitem
         end
         # sort and add platforms
-        platforms.sort_by! { |i| i[:total_activity] }.reverse!
+        platforms.sort_by! {|i| i[:total_activity] }.reverse!
         processed_result[:platforms] = platforms
 
         # process runtimes
@@ -118,12 +111,13 @@ module Lanalytics
           result_subitem = {}
           result_subitem[:runtime] = runtime['key']
           result_subitem[:total_activity] = runtime['doc_count']
-          result_subitem[:relative_activity] = runtime['doc_count'].percent_of total_activity
+          result_subitem[:relative_activity] =
+            runtime['doc_count'].percent_of(total_activity)
 
           runtimes << result_subitem
         end
         # sort and add runtimes
-        runtimes.sort_by! { |i| i[:total_activity] }.reverse!
+        runtimes.sort_by! {|i| i[:total_activity] }.reverse!
         processed_result[:runtimes] = runtimes
 
         # evaluate behavior
@@ -131,32 +125,29 @@ module Lanalytics
           {
             category: 'desktop web',
             total_activity: desktop_web,
-            relative_activity: desktop_web.percent_of(total_activity)
+            relative_activity: desktop_web.percent_of(total_activity),
           },
           {
             category: 'mobile web',
             total_activity: mobile_web,
-            relative_activity: mobile_web.percent_of(total_activity)
+            relative_activity: mobile_web.percent_of(total_activity),
           },
           {
             category: 'mobile app',
             total_activity: mobile_app,
-            relative_activity: mobile_app.percent_of(total_activity)
+            relative_activity: mobile_app.percent_of(total_activity),
           },
         ]
 
         # sort usage
-        usage.sort_by! { |i| i[:total_activity] }.reverse!
+        usage.sort_by! {|i| i[:total_activity] }.reverse!
 
         # add behavior
-        behavior = {
-          usage: usage
-        }
+        behavior = {usage: usage}
         processed_result[:behavior] = behavior
 
         processed_result
       end
-
     end
   end
 end
