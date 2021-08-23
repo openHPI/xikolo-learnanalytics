f2a31174aa50de9596c24a117b729bcc218ba0ea
Tobias Rohloff <tobias.rohloff@hpi.de>
2021-08-23 11:42:54 +0200

    Please rubocop

--- lib/lanalytics/metric/video_play_count.rb
+++ lib/lanalytics/metric/video_play_count.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 module Lanalytics
   module Metric
     class VideoPlayCount < ExpEventsElasticMetric
-
       description 'Videos played by users.'
 
       required_parameter :course_id
@@ -12,40 +13,39 @@ module Lanalytics
           query: {
             bool: {
               must: [
-                { match: { verb: 'video_play' } },
-              ] + all_filters(nil, params[:course_id], nil)
-            }
+                {match: {verb: 'video_play'}},
+              ] + all_filters(nil, params[:course_id], nil),
+            },
           },
           aggs: {
             items: {
               terms: {
                 field: 'resource.resource_uuid',
-                size: 1_000
+                size: 1_000,
               },
               aggs: {
                 user: {
                   cardinality: {
                     field: 'user.resource_uuid',
-                    precision_threshold: 40_000
-                  }
-                }
-              }
+                    precision_threshold: 40_000,
+                  },
+                },
+              },
             },
             sum: {
               sum_bucket: {
-                buckets_path: 'items>user'
-              }
-            }
-          }
+                buckets_path: 'items>user',
+              },
+            },
+          },
         }
 
         result = datasource.exec do |client|
           client.search index: datasource.index, body: body
         end
 
-        { count: result.dig('aggregations', 'sum', 'value').to_i }
+        {count: result.dig('aggregations', 'sum', 'value').to_i}
       end
-
     end
   end
 end
