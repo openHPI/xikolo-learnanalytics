fbc3cee15b529f01eeb03d1daea0ff4b8b982d75
Dominic Sauer <dominic.sauer@hpi.de>
2023-06-10 00:08:21 +0200

    Ignore redis client connection errors in Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
+  # Far too many exception when sidekiq workers are just reconnecting
+  config.excluded_exceptions += %w[Redis::CannotConnectError RedisClient::CannotConnectError]
+
   # Do not send full list of gems with each event
   config.send_modules = false
 end
