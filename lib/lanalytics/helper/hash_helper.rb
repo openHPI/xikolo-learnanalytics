06af1f9e67c7b0c9e7ae119e861a24552abec815
Dominic Sauer <dominic.sauer@hpi.de>
2023-07-26 09:24:51 +0200

    rubocop: Fix Style/HashConversion

--- lib/lanalytics/helper/hash_helper.rb
+++ lib/lanalytics/helper/hash_helper.rb
@@ -2,8 +2,8 @@
 
 module Lanalytics::Helper::HashHelper
   # http://stackoverflow.com/questions/8706930/converting-nested-hash-keys-from-camelcase-to-snake-case-in-ruby
-  def underscore_key(k)
-    k.to_s.underscore.to_sym
+  def underscore_key(key)
+    key.to_s.underscore.to_sym
   end
 
   def hash_keys_to_underscore(value)
@@ -11,7 +11,7 @@ module Lanalytics::Helper::HashHelper
       when Array
         value.map {|v| hash_keys_to_underscore(v) }
       when Hash
-        Hash[value.map {|k, v| [underscore_key(k), hash_keys_to_underscore(v)] }]
+        value.to_h {|k, v| [underscore_key(k), hash_keys_to_underscore(v)] }
       else
         value
     end
