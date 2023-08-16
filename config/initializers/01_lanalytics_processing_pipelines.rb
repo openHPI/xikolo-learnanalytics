754fec2f757e0aba27c03cb4f26dd878d1ab0aec
Jan Graichen <jan.graichen@hpi.de>
2023-08-16 22:07:53 +0200

    chore: Upgrade to Ruby 3.2

--- config/initializers/01_lanalytics_processing_pipelines.rb
+++ config/initializers/01_lanalytics_processing_pipelines.rb
@@ -7,11 +7,7 @@ require 'lanalytics/processing/datasource_manager'
 require 'lanalytics/processing/pipeline_manager'
 
 LANALYTICS_PIPELINE_FLIPPER_FILE = "#{Rails.root}/config/lanalytics_pipeline_flipper.yml"
-begin
-  flipper_config = YAML.load_file(LANALYTICS_PIPELINE_FLIPPER_FILE, aliases: true).with_indifferent_access
-rescue ArgumentError # Ruby 2.7 does not has aliases: keyword
-  flipper_config = YAML.load_file(LANALYTICS_PIPELINE_FLIPPER_FILE).with_indifferent_access
-end
+flipper_config = YAML.load_file(LANALYTICS_PIPELINE_FLIPPER_FILE, aliases: true).with_indifferent_access
 flipper_config = flipper_config[Rails.env] || flipper_config
 
 # Load all datasources
