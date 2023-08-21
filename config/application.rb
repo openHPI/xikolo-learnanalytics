942ecfc4b703d58a504fbb9c6b1678c339f39085
Dominic Sauer <dominic.sauer@hpi.de>
2023-08-21 21:34:55 +0200

    fix: Update the bundler version

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 
-require 'bundler'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
