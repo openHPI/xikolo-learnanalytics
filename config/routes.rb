5c16ff98b89991861621b2e1dfb2b2274543f57f
Dominic Sauer <dominic.sauer@hpi.de>
2025-02-14 11:34:58 +0100

    chore: Consistent routes
    Align the order of routes with the core.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@
 
 Rails.application.routes.draw do
   defaults format: :json do
-    root to: 'root#index'
-
-    resources :system_info, only: [:show]
-
     # Metrics
     get 'metrics/:name', to: 'metrics#show', as: :metric
     get 'metrics', to: 'metrics#index'
@@ -16,5 +12,8 @@ Rails.application.routes.draw do
 
     # Statistics
     resources :course_statistics, only: %i[index show]
+
+    resources :system_info, only: %i[show]
+    root to: 'root#index'
   end
 end
